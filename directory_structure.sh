41d1ca36e8362f458f7a3aed134a1c259a160897
hoomen <hoomen@student.42heilbronn.de>
2023-03-08 17:57:48 +0100

    directory structure

---
+++ directory_structure.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Remove old project folder if it existed
+if [ -d project ]
+then
+	echo "Removing old project folder ..."
+	rm -drf project
+fi	
+echo "Creating directory structure ..."
+
+# Create basic folder structure and print
+basic="project/srcs/requirements"
+mkdir -p $basic
+
+# Create makefile & gitignore
+cd project
+touch Makefile
+
+# Create docker compose file & .env
+cd srcs
+touch docker-compose.yml .env
+
+# Create subfolders for the docker containers
+cd requirements
+requirements="nginx mariadb wordpress"
+for container in $requirements
+do
+	mkdir $container
+	cd $container
+	touch Dockerfile
+	mkdir tools
+	mkdir conf
+	touch tools/.gitkeep
+	touch conf/.gitkeep
+	cd ..
+done
+cd ../..
+
+# Show directory structure
+echo "ls -laR -I.git"
+ls -laR
+
+#Copy this script to the project folder
+cp ../directory_structure.sh .
