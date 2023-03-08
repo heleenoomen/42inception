4686b310f3f130403f16f269bdcbc99a72d2963a
hoomen <hoomen@student.42heilbronn.de>
2023-03-08 18:58:43 +0100

    how to ssh

---
+++ setup/scripts/git_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd project
+
+git init
+
+git config --global init.defaultBranch main
+git config user.email hoomen@student.42heilbronn.de
+git config user.name hoomen
+
+touch .gitignore
+echo ".gitignore" >> .gitignore
+echo "**/.env" >> .gitignore
+
+git add .
+git commit -m "directory structure"
+
+
