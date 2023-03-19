28aed728d56eaca3b21a048a988f440a00ed3918
hoomen <hoomen@student.42heilbronn.de>
2023-03-19 20:56:21 +0100

    mariadb comments

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 
-# make a directory where mariadb can acces the myssqld.sock socket file
+# make a directory where mariadb can acces the mysqld.sock socket file
 mkdir /var/run/mysqld
 
 # change the config file so TCP/IP is no longer disabled
@@ -9,22 +9,22 @@ sed -i "s/skip-networking/skip-networking=0/g" /etc/my.cnf.d/mariadb-server.cnf
 # change the config file so that bind-address=0.0.0.0 is no longer commented out (this means mariadb can now bind to any address)
 sed -i "s/#bind-address/bind-address/g" /ect/my.cnf.d/mariadb-server.cnf
 
-# check if the database was already initialized. (In that case, the directory /var/lib/mysql/mysql will exist. If not, initialize the database and tell it to stort its tables in /var/lib/mysql
+# check if the system tables where already created, in which cases the directory /var/lib/mysql/mysql will exist. Ifit does not exist, install the system tables and store the data in /var/lib/mysql
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
         # init database
-        mysql_install_db --datadir=/var/lib/mysqld
+        mysql_install_db --datadir=/var/lib/mysql
 
 fi
 
-# check if the wordpress database exists. If not yet, make a create_db.sql script to (1) secure the database and (2) add the wordpress database.
+# check if the wordpress database exists. If not yet, create a temporary create_db.sql script to (1) secure the database and (2) add the wordpress database.
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
         cat << EOF > /tmp/create_db.sql
 FLUSH PRIVILEGES;
 DELETE FROM     mysql.user WHERE User='';
 DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.db WHERE Db='test' or 'test\_%';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
